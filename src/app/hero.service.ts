1c7a05b5e8d84bf2fbfd74c7c261b17885c3af54
TACCOEN <theo.taccoen@cgi.com>
2022-04-06 10:28:03 +0200

    partie navigation finie

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,4 +15,10 @@ export class HeroService {
     this.messageService.addMessage('heroService : heroes fetched');
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.addMessage(`HeroService : fetched hero id=${id}`);
+    return of(hero);
+  }
 }
